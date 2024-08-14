2a01c30e6d0c2cb3c1baf74149c7dd4809391c7f
chitra.m.saraswati@gmail.com <13806861+chitrams@users.noreply.github.com>
2024-08-14 16:20:28 +0800

    First steps update

--- chitra/nick_0808.R
+++ chitra/nick_0808.R
@@ -1,6 +1,6 @@
 # Issue 151 - https://github.com/idem-lab/conmat/issues/151
 # So what we want to do is take a different survey that isn't POLYMOD
-# and tidy the data into a format that can be used with 
+# and tidy the data into a format that can be used with
 # `fit_single_contact_model()`
 # An example workflow is shown below where are using already tidied up
 # data from polymod, which is provided with:
@@ -36,7 +36,7 @@ my_mod <- fit_single_contact_model(
   population = example_population
 )
 
-# so what we want to do is take some other contact survey data and 
+# so what we want to do is take some other contact survey data and
 # get it into this format:
 #### setting (settings - home, school, work, other)
 #### age_from: The age of the people who did the survey
@@ -57,7 +57,7 @@ zim <- socialmixr::get_survey(survey = "https://doi.org/10.5281/zenodo.3886638")
 clean_zim <- clean(zim)
 clean_zim$participants
 
-# instead, I'd recommend exploring the data cleaning steps from 
+# instead, I'd recommend exploring the data cleaning steps from
 # `get_polymod_contact_data()`
 # and seeing if they relate back to some of the socialmixr data
 # feel free to copy the code from `get_polymod_contact_data()`
@@ -65,15 +65,15 @@ clean_zim$participants
 
 # A few goals
 # 1. Get a dataset from socialmixr into the right format to then do
-#  `fit_single_contact_model()` on it (using relevant population data. e.g., 
+#  `fit_single_contact_model()` on it (using relevant population data. e.g.,
 # if you get a social contact survey from Zimbabwe, use census/age data from
 # zimbabwe for the `population` argument in `fit_single_contact_model`)
 # 2. Write out step 1 into a vignette, unpacking the data cleaning process
 # And then also using `fit_single_contact_model` - and extending this
 # to use other settings - work, school, other, all.
 # And then explore using `fit_setting_contacts()`
-# (stretch goal :2. Consider a way to turn our data cleaning steps in 
+# (stretch goal :2. Consider a way to turn our data cleaning steps in
 # `get_polymod_contact_data` into another function that can tidy up a given
 # dataset from socialmixr. Don't get too bogged down - there's a lot of contact
-# surveys. This might also be similar to what `sociamixr::clean()` does, 
+# surveys. This might also be similar to what `sociamixr::clean()` does,
 # so that might also be worthwhile exploring?
--- chitra/other-surveys.R
+++ chitra/other-surveys.R
@@ -19,8 +19,8 @@ china_survey <- get_survey("https://doi.org/10.5281/zenodo.3878754")
 
 # There are some errors.
 saveRDS(somaliland_survey, "./data/somaliland_survey.rda")
-saveRDS(thailand_survey, "./data/thailand_survey.rda")
-saveRDS(china_survey, "./data/china_survey.rda")
+save(thailand_survey, "./data/thailand_survey.rda")
+save(china_survey, "./data/china_survey.rda")
 
 # Similar to POLYMOD
 # BE, CH, NL, UK during Covid
@@ -38,3 +38,95 @@ saveRDS(comixv2_survey, "./data/comixV2.rda")
 # To start small:
 # - China 640.7 kb
 # - Thailand 1.5 MB
+
+# Set-up -----
+
+#%% Load packages ------
+
+library(socialmixr)
+library(conmat)
+library(tidyverse)
+
+#%% Load test data -----
+
+china_contact <- readRDS("./data/china_survey.rda")
+comix_contact <- readRDS("./data/comix_survey.rda")
+thailand_contact <- readRDS("./data/thailand_survey.rda")
+
+#%% Load our ground truth -----
+
+example_contact <- get_polymod_contact_data(setting = "home")
+
+# Try -----
+
+#%% Select contact data only for China ----
+
+contact_data <- china_contact$contacts
+
+#%% Cleaning 1 ------
+
+# impute contact ages according to the required method
+
+contact_data_imputed <- contact_data %>%
+  dplyr::mutate(
+    cnt_age_sampled = floor(
+      # suppress warnings about NAs in runif
+      suppressWarnings(
+        stats::runif(
+          n = dplyr::n(),
+          min = cnt_age_est_min,
+          max = cnt_age_est_max + 1
+        )
+      )
+    ),
+    cnt_age_mean = floor(
+      cnt_age_est_min + (cnt_age_est_max + 1 - cnt_age_est_min) / 2
+    ),
+    cnt_age = dplyr::case_when(
+      !is.na(cnt_age_exact) ~ as.numeric(cnt_age_exact),
+      TRUE ~ NA_real_
+    )
+  )
+
+participant_info <- china_contact$participants %>% 
+  select(part_id, part_age, part_gender)
+
+contact_data_participant_info <- left_join(
+  contact_data_imputed, participant_info, by = "part_id"
+)
+
+# filter out any participants with missing contact ages or settings (can't
+# just remove the contacts as that will bias the count)
+contact_data_filtered <- contact_data_participant_info %>%
+  dplyr::group_by(part_id) %>%
+  dplyr::mutate(
+    missing_any_contact_age = any(is.na(cnt_age_exact)),
+    missing_any_contact_setting = any(
+        is.na(cnt_home) |
+        is.na(cnt_work) |
+        is.na(cnt_school) |
+        is.na(cnt_transport) |
+        # is.na(cnt_leisure) |
+        is.na(cnt_otherplace)
+    )
+  ) %>%
+  dplyr::ungroup() %>%
+  dplyr::filter(
+    !is.na(part_age),
+    !missing_any_contact_age,
+    !missing_any_contact_setting
+  )
+
+# Create new setting column here?
+
+# get contacts by setting (keeping 0s, so we can record 0 contacts for some individuals)
+contact_data_setting <- contact_data_filtered %>%
+  dplyr::mutate(
+    contacted = dplyr::case_when(
+      setting == "all" ~ 1L,
+      setting == "home" ~ cnt_home,
+      setting == "school" ~ cnt_school,
+      setting == "work" ~ cnt_work,
+      setting == "other" ~ pmax(cnt_transport, cnt_otherplace),
+    )
+  )
