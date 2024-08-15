3bd6427ed7a205c19136fba51d1feab3ac3a35cd
chitra.m.saraswati@gmail.com <13806861+chitrams@users.noreply.github.com>
2024-08-15 12:19:06 +0800

    Updated wrangling to get cleaned data ready for extrapolate_polymod, not quite there yet

--- chitra/other-surveys.R
+++ chitra/other-surveys.R
@@ -1,9 +1,24 @@
+# Set-up -----
+
+#%% Load packages ------
+
+library(socialmixr)
+library(conmat)
+library(tidyverse)
+
+#%% Download and explore POLYMOD (control) ----
+
 load("./data/polymod.rda")
 
 View(polymod[["participants"]])
 View(polymod[["contacts"]])
 
-library(socialmixr)
+polymod_contact <- polymod$contacts
+
+home_contact <- get_polymod_contact_data(setting = "home")
+
+#%% Download and explore other surveys ----
+
 other_surveys <- socialmixr::list_surveys()
 
 # Different household structures;
@@ -17,10 +32,8 @@ somaliland_survey <- get_survey("https://doi.org/10.5281/zenodo.7071876")
 thailand_survey <- get_survey("https://doi.org/10.5281/zenodo.4086739")
 china_survey <- get_survey("https://doi.org/10.5281/zenodo.3878754")
 
-# There are some errors.
-saveRDS(somaliland_survey, "./data/somaliland_survey.rda")
-save(thailand_survey, "./data/thailand_survey.rda")
-save(china_survey, "./data/china_survey.rda")
+saveRDS(thailand_survey, "./data/thailand_survey.rda")
+saveRDS(china_survey, "./data/china_survey.rda")
 
 # Similar to POLYMOD
 # BE, CH, NL, UK during Covid
@@ -30,40 +43,28 @@ saveRDS(comix_survey, "./data/comix_survey.rda")
 # CoMix 2.0 arguably has a greater "mix" of the types of households?
 # AT, BE, DK, HR (Croatia), EE (Estonia), GR, IT, PL, PT over covid
 comixv2_survey <- get_survey("https://zenodo.org/records/7331926")
-saveRDS(comixv2_survey, "./data/comixV2.rda")
+saveRDS(comixv2_survey, "./data/comixV2_survey.rda")
 # But there are issues: warning messages with merging. 
 
-# China, Thailand and comix might be the least problematic to use.
+# China, Thailand and comix (V2) might be the least problematic to use.
 # No warning messages when "compiling", whereas Somaliland had issues.
 # To start small:
 # - China 640.7 kb
 # - Thailand 1.5 MB
 
-# Set-up -----
-
-#%% Load packages ------
-
-library(socialmixr)
-library(conmat)
-library(tidyverse)
-
 #%% Load test data -----
 
-china_contact <- readRDS("./data/china_survey.rda")
-comix_contact <- readRDS("./data/comix_survey.rda")
-thailand_contact <- readRDS("./data/thailand_survey.rda")
-
-#%% Load our ground truth -----
-
-example_contact <- get_polymod_contact_data(setting = "home")
+china_survey <- readRDS("./data/china_survey.rda")
+comix_survey <- readRDS("./data/comixv2_survey.rda")
+thailand_survey <- readRDS("./data/thailand_survey.rda")
 
 # Try -----
 
-#%% Select contact data only for China ----
+#%% First clean, China ------
 
-contact_data <- china_contact$contacts
+# Select contact data only for China
 
-#%% Cleaning 1 ------
+contact_data <- china_survey$contacts
 
 # impute contact ages according to the required method
 
@@ -88,7 +89,7 @@ contact_data_imputed <- contact_data %>%
     )
   )
 
-participant_info <- china_contact$participants %>% 
+participant_info <- china_survey$participants %>% 
   select(part_id, part_age, part_gender)
 
 contact_data_participant_info <- left_join(
@@ -117,16 +118,37 @@ contact_data_filtered <- contact_data_participant_info %>%
     !missing_any_contact_setting
   )
 
-# Create new setting column here?
+#TODO Create new setting column here?
+# Sum all contacts,
+# grouping by age to and age from,
+# for each setting. 
 
 # get contacts by setting (keeping 0s, so we can record 0 contacts for some individuals)
+temp <- contact_data_filtered %>%
+  dplyr::mutate(contacted = cnt_home
+  )
+
 contact_data_setting <- contact_data_filtered %>%
-  dplyr::mutate(
-    contacted = dplyr::case_when(
-      setting == "all" ~ 1L,
-      setting == "home" ~ cnt_home,
-      setting == "school" ~ cnt_school,
-      setting == "work" ~ cnt_work,
-      setting == "other" ~ pmax(cnt_transport, cnt_otherplace),
-    )
+  
+  #TODO Add a group by here? home, work, etc (contacted) before next cleaning steps
+  
+  dplyr::select(
+    part_id,
+    age_from = part_age,
+    age_to = cnt_age,
+    contacted
+  ) %>%
+  tidyr::complete(
+    tidyr::nesting(age_from, part_id),
+    age_to = ages,
+    fill = list(contacted = 0)
+  ) %>%
+  dplyr::group_by(
+    age_from,
+    age_to
+  ) %>%
+  dplyr::summarise(
+    contacts = sum(contacted),
+    participants = dplyr::n_distinct(part_id),
+    .groups = "drop"
   )
